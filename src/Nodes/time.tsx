95cb86c0ba3ab92a40d8b375ab525783fd22ea44
Chris Neale <chris@komododigital.co.uk>
2019-02-13 08:11:07 +0000

    Using toFixed instead of toPrecision

--- src/Nodes/time.tsx
+++ src/Nodes/time.tsx
@@ -37,7 +37,7 @@ export function Time({ id, x, y, output }: iTime) {
   const [connect, setConnect] = useState(false);
 
   useEffect(() => {
-    output(value.toPrecision(3));
+    output(value.toFixed(3));
   });
 
   useAnimationFrame(() => {
@@ -47,7 +47,7 @@ export function Time({ id, x, y, output }: iTime) {
   });
 
   const io = (
-    <Output key={`${id}-o-0`} id={`${id}-o-0`} value={value.toPrecision(3)} />
+    <Output key={`${id}-o-0`} id={`${id}-o-0`} value={value.toFixed(3)} />
   );
 
   const controls = <button onClick={() => setPause(!pause)}>Pause</button>;
