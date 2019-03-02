3e513d5ed0a43e6e54acc9c9a561e3ba0a439020
Chris Neale <chris@komododigital.co.uk>
2019-03-02 18:13:23 +0000

    Drag panels started

--- src/Components/panel.tsx
+++ src/Components/panel.tsx
@@ -10,9 +10,29 @@ interface iPanel {
 
 export function Panel({ title, x, y, io, controls }: iPanel) {
   const panelRef = useRef(null);
+  const [dragging, setDragging] = useState(false);
+  const [initPos, setInitPos] = useState(null);
+  const [pos, setPos] = useState({ x, y });
   return (
-    <div ref={panelRef} className="Panel" style={{ top: y, left: x }}>
-      <div className="Title">{title}</div>
+    <div ref={panelRef} className="Panel" style={{ top: pos.y, left: pos.x }}>
+      <div
+        className="Title"
+        onMouseDown={e => {
+          setDragging(true);
+          setInitPos({ x: e.clientX, y: e.clientY });
+        }}
+        onMouseUp={() => setDragging(false)}
+        onMouseMove={e => {
+          if (dragging) {
+            const deltaX = e.clientX - initPos.x;
+            const deltaY = e.clientY - initPos.y;
+
+            setPos({ x: x + deltaX, y: y + deltaY });
+          }
+        }}
+      >
+        {title}
+      </div>
       <div className="IO">{io}</div>
       <p className="Description">{controls}</p>
     </div>
