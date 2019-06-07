04247ecca903ee759c4792a91cfa50413f8a765e
Chris Neale <chris@komododigital.co.uk>
2019-06-07 08:16:14 +0100

    Drag code on panels working

--- src/Components/panel.tsx
+++ src/Components/panel.tsx
@@ -29,6 +29,7 @@ export function Panel({ id, title, x, y, io, controls }: iPanel) {
   const panelRef = useRef(null);
   const [dragging, setDragging] = useState(false);
   const [initPos, setInitPos] = useState({ x: 0, y: 0 });
+  const [delta, setDelta] = useState({ x: 0, y: 0 });
   const [pos, setPos] = useState({ x, y });
 
   useEffect(() => {
@@ -46,20 +47,27 @@ export function Panel({ id, title, x, y, io, controls }: iPanel) {
     if (dragging) {
       const deltaX = mouseX - initPos.x;
       const deltaY = mouseY - initPos.y;
-      setPos({ x: x + deltaX, y: y + deltaY });
+      setDelta({ x: deltaX, y: deltaY });
     }
-  });
+  }, [dragging, mouseX, mouseY]);
 
   return (
-    <div ref={panelRef} className="Panel" style={{ top: pos.y, left: pos.x }}>
+    <div
+      ref={panelRef}
+      className="Panel"
+      style={{ top: pos.y + delta.y, left: pos.x + delta.x }}
+    >
       <div
         className="Title"
         onMouseDown={(e: React.MouseEvent) => {
           setDragging(true);
           setInitPos({ x: e.clientX, y: e.clientY });
+          console.log("down", initPos, pos);
         }}
-        onMouseUp={() => {
+        onMouseUp={e => {
           setDragging(false);
+          setPos({ x: pos.x + delta.x, y: pos.y + delta.y });
+          setDelta({ x: 0, y: 0 });
           dispatch({
             type: "panelMove",
             id: id,
