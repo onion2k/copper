ecf478e8c8e49843a63b041eaac2d6398588ff4d
Chris Neale <chris@komododigital.co.uk>
2019-06-19 13:12:31 +0100

    Deleted commented code

--- src/Panels/shader.tsx
+++ src/Panels/shader.tsx
@@ -111,29 +111,3 @@ export default function Arithmatic({ id, x, y }: iShader) {
     />
   );
 }
-
-// const gl = document.getElementById("c").getContext("webgl");
-// const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
-
-// const arrays = {
-//   position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
-// };
-// const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
-
-// function render(time) {
-//   twgl.resizeCanvasToDisplaySize(gl.canvas);
-//   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-//   const uniforms = {
-//     time: time * 0.001,
-//     resolution: [gl.canvas.width, gl.canvas.height],
-//   };
-
-//   gl.useProgram(programInfo.program);
-//   twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-//   twgl.setUniforms(programInfo, uniforms);
-//   twgl.drawBufferInfo(gl, bufferInfo);
-
-//   requestAnimationFrame(render);
-// }
-// requestAnimationFrame(render);
